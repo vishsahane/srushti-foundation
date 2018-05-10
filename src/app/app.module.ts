ea48b2cf543075e063e8426de5e77c1e969411c1
Sahane, Vishwas <Vishwas.Sahane@jetblue.com>
2018-05-10 11:51:20 +0530

    Merged header & footer componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,11 @@ import { AppComponent } from './app.component';
 
 import { MaterialModule } from './material.module';
 import { RoutingModule } from './routing.module';
-import { HeaderComponent } from '../components/header/header.component';
-import { FooterComponent } from '../components/footer/footer.component';
 import { AboutUsComponent } from '../components/about-us/about-us.component';
 import { ContactUsComponent } from '../components/contact-us/contact-us.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     ContactUsComponent,
     AboutUsComponent
   ],
