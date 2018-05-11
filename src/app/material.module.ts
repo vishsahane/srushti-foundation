05e30ce534f6db8c324a4c9b7b562f4712217828
Vishwas Sahane <sahanevishwas@yahoo.com>
2018-05-11 16:22:40 +0530

    gallery added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,11 +5,12 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   imports: [MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule, MatSidenavModule,
-    MatIconModule, MatListModule],
+    MatIconModule, MatListModule, MatCardModule],
   exports: [MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule, MatSidenavModule,
-    MatIconModule, MatListModule],
+    MatIconModule, MatListModule, MatCardModule],
 })
 export class MaterialModule { }
